06922d8a79b5cbd9d3523940ac6f2b83b1a14384
bentevissel <univ5946@ox.ac.uk>
2025-10-27 17:53:40 +0000

    fixed issue with hysplit params - can add now. added output from pysplit-test in folder trajectories

--- Pysplit-test.py
+++ Pysplit-test.py
@@ -13,13 +13,13 @@
 # 'edas.jan2007_2'.
 
 # Import the package
-
+import os
 import pysplit
 
 #Working directory
 working_dir = "/home/bvissel/airflow/hysplit5.4/working/"
 #location where it will be stored
-storage_dir = "/home/bvissel/airflow/trajectories/colgate2/"
+storage_dir = "/home/bvissel/airflow-trajectories/trajectories/colgate/"
 #data dir
 meteo_dir = "/home/bvissel/airflow/data/"
 
@@ -28,6 +28,30 @@ hysplit_executable = "/home/bvissel/airflow/hysplit5.4/exec/hyts_std"
 # in order for this to work also need to enter in terminal: 
 # ls -l /home/bvissel/airflow/hysplit5.4/exec/hyts_std
 # chmod +x /home/bvissel/airflow/hysplit5.4/exec/hyts_std
+
+os.makedirs(storage_dir, exist_ok=True)
+
+#create a setup file
+setup_cfg_path = os.path.join(working_dir, "SETUP.CFG")
+
+#define which params you want
+setup_content = """&SETUP
+tm_tpot = 1,     ! Potential temperature
+tm_tamb = 1,     ! Ambient temperature
+tm_rain = 1,     ! Precipitation
+tm_mixd = 1,     ! Mixing depth
+tm_sphu = 1,     ! Specific humidity
+tm_pres = 1,     ! Pressure
+tm_relh = 1,     ! Relative humidity
+/
+"""
+
+#create (overwrite) setup file
+with open(setup_cfg_path, "w") as f:
+    f.write(setup_content)
+
+#confirm creation setup file
+print(f"SETUP.CFG written to {setup_cfg_path}")
 
 #basename
 basename = 'colgate'
@@ -40,7 +64,8 @@ months = [8]
 hours = [11, 17, 23]
 altitudes = [500, 1000, 1500]
 location = (42.82, -75.54)
-runtime = -120
+#with a runtime of -120, it has to contain the july week before
+runtime = -72
 
 
 #run function
